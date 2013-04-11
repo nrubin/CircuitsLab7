9b0dd441f48a121050d76b1a8c6b32e53f5dcba8
Noam Rubin <rubin.m.noam@gmail.com>
2013-04-10 23:08:10 -0400

    fixed some stuff on the big plot

--- Vdm_vs_I.m
+++ Vdm_vs_I.m
@@ -1,5 +1,5 @@
 %Experiment 1 plotter for Vb = 0.5, V2 = 1V
-interval = 10;
+interval = 1;
 
 load('newdata.mat')
 
@@ -21,22 +21,21 @@ Icm3 = I1iii + I2iii;
 I1_3 = I1iii;      
 I2_3 = I2iii;      
 
-plot(Vdm1(1:interval:end),I1_1(1:interval:end),'*--','MarkerSize',15,'LineWidth',2); hold on;
-plot(Vdm2(1:interval+1:end),I1_2(1:interval+1:end),'r*--','MarkerSize',15,'LineWidth',2); 
-plot(Vdm3(1:interval-1:end),I1_3(1:interval-1:end),'g*--','MarkerSize',15,'LineWidth',2);
+plot(Vdm1(1:interval:end),I1_1(1:interval:end),'*','MarkerSize',10); hold on;
+plot(Vdm1(1:interval:end),I2_1(1:interval:end),'s','MarkerSize',10);
+plot(Vdm1(1:interval:end),Idm1(1:interval:end),'^','MarkerSize',10);
+plot(Vdm1(1:interval:end),Icm1(1:interval:end),'o','MarkerSize',10);
 
-plot(Vdm1(1:interval:end),I2_1(1:interval:end),'s--','MarkerSize',15,'LineWidth',2);
-plot(Vdm1(1:interval:end),Idm1(1:interval:end),'^--','MarkerSize',15,'LineWidth',2);
-plot(Vdm1(1:interval:end),Icm1(1:interval:end),'o--','MarkerSize',15,'LineWidth',2);
 
-plot(Vdm2(1:interval+1:end),I2_2(1:interval+1:end),'rs--','MarkerSize',15,'LineWidth',2);
-plot(Vdm2(1:interval+1:end),Idm2(1:interval+1:end),'r^--','MarkerSize',15,'LineWidth',2);
-plot(Vdm2(1:interval+1:end),Icm2(1:interval+1:end),'ro--','MarkerSize',15,'LineWidth',2);
+plot(Vdm2(1:interval:end),I1_2(1:interval:end),'r*','MarkerSize',10); 
+plot(Vdm2(1:interval:end),I2_2(1:interval:end),'rs','MarkerSize',10);
+plot(Vdm2(1:interval:end),Idm2(1:interval:end),'r^','MarkerSize',10);
+plot(Vdm2(1:interval:end),Icm2(1:interval:end),'ro','MarkerSize',10);
 
- 
-plot(Vdm3(1:interval-1:end),I2_3(1:interval-1:end),'gs--','MarkerSize',15,'LineWidth',2);
-plot(Vdm3(1:interval-1:end),Idm3(1:interval-1:end),'g^--','MarkerSize',15,'LineWidth',2);
-plot(Vdm3(1:interval-1:end),Icm3(1:interval-1:end),'go--','MarkerSize',15,'LineWidth',2);
+plot(Vdm3(1:interval:end),I2_3(1:interval:end),'gs','MarkerSize',10);
+plot(Vdm3(1:interval:end),I1_3(1:interval:end),'g*','MarkerSize',10);
+plot(Vdm3(1:interval:end),Idm3(1:interval:end),'g^','MarkerSize',10);
+plot(Vdm3(1:interval:end),Icm3(1:interval:end),'go','MarkerSize',10);
 
 xlim([-0.3 0.3]);
 grid on;
